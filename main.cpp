f5394de8bf9b3be0f44685ec903e0a805204b5db
Charles Notley <chnotley@gmail.com>
2019-07-19 18:15:24 -0700

    Removed input loop

--- main.cpp
+++ main.cpp
@@ -5,19 +5,18 @@ int main()
 {
     unsigned int count = 0;
 
-    do
+    std::cout << "Program displays the first n numbers in the Fibonacci Sequence.\n";
+    std::cout << "Enter number of Fibonaccci numbers to display (50 maximum):\n";
+    std::cin >> count;
+  
+    if(count > 50)
     {
-        std::cout << "Program displays the first n numbers in the Fibonacci Sequence.\n";
-        std::cout << "Enter number of Fibonaccci numbers to display (50 maximum):\n";
-        std::cin >> count;
-  
-        if(count > 50)
-        {
-	    std::cout << count << " is too large.  Maximum number of Fibonacci numbers is 50.\n";
-        }
-  
-    }while(0 == count || count > 50);
+        std::cout << count << " is too large.  Maximum number of Fibonacci numbers is 50.\n";
+    }
+    else
+    {
+        std::cout << count << '\n';
+    }
 
-    std::cout << count << '\n';
     return 0;
 }
